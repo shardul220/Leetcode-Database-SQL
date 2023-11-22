28441f1df1117544687af3463ecbf8f834e798c4
shardul220 <shardul220@gmail.com>
2023-11-22 00:28:06 -0500

    Time: 822 ms (80.21%) | Memory: 27.5 MB (6.57%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -11,12 +11,25 @@ class Solution:
         # print(maxdiff)
         # return maxdiff
 
-        minprice=float('inf')
-        maxprofit=0
-        for i in range(l):
-            if prices[i]<minprice:
-                minprice=prices[i]
-            diff = prices[i]-minprice
-            if diff > maxprofit:
-                maxprofit = diff
-        return maxprofit
+        # minprice=float('inf')
+        # maxprofit=0
+        # for i in range(l):
+        #     if prices[i]<minprice:
+        #         minprice=prices[i]
+        #     diff = prices[i]-minprice
+        #     if diff > maxprofit:
+        #         maxprofit = diff
+        # return maxprofit
+
+        left=0
+        r=1
+        maxprof = 0
+        while r<l:
+            if prices[r]<=prices[left]:
+                left=r
+            else:
+                diff=prices[r]-prices[left]
+                if diff> maxprof:
+                    maxprof = diff
+            r+=1
+        return maxprof
