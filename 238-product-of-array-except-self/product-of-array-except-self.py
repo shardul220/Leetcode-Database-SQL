32608b96b9efac4ed7be4d0d984e75ec90b3868b
shardul220 <shardul220@gmail.com>
2023-11-23 18:07:25 -0500

    Time: 193 ms (63.21%) | Memory: 23.9 MB (73.85%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,12 +1,7 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         new = [0]*len(nums)
-        # tot_product = 1
-        # for num in nums:
-        #     tot_product *= num
 
-        # for num in nums:
-        #     new.append(tot_product/num) 
         # #Bruteforce
         # for i,num in enumerate(nums):
         #     product = 1
@@ -17,21 +12,32 @@ class Solution:
         # return new  
 
         n=len(nums)
-        l = [0]*n
-        r = [0]*n
+        # l = [0]*n
+        # r = [0]*n
 
-        l[0] = 1
+        # l[0] = 1
 
-        for i in range(1,n):
-            l[i] = l[i-1]*nums[i-1]
+        # for i in range(1,n):
+        #     l[i] = l[i-1]*nums[i-1]
 
-        r[n-1] = 1
-        for i in reversed(range(n-1)):
-            r[i] = nums[i+1] * r[i+1]
+        # r[n-1] = 1
+        # for i in reversed(range(n-1)):
+        #     r[i] = nums[i+1] * r[i+1]
 
+        # for i in range(n):
+        #     new[i] = l[i]*r[i]
+        # return new
+
+        res = [1]*n
+        prefix=1
         for i in range(n):
-            new[i] = l[i]*r[i]
-        return new
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix=1
+        for i in range(n-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
 
 
         
